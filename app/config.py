80b737062ebccec43e7226b909f6516ead54b1c6
Brandon Tsui <itstsui@gmail.com>
2020-06-11 01:18:27 -0400

    heroku deployment changes

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 
 class Configuration:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
