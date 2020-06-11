7cc554925365bcb7caa5fd1d96a5958e5e378279
Brandon Tsui <itstsui@gmail.com>
2020-06-11 01:34:52 -0400

    heroku deployment changes

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 
-class Configuration:
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
